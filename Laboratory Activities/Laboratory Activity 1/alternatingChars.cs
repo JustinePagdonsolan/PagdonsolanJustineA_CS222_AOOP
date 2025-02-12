90e2a7c00958e010b5d7fffc8f3862d4af3e0a42
JustinePagdonsolan <164887230+JustinePagdonsolan@users.noreply.github.com>
2025-02-13 07:02:42 +0800

    Add files via upload

---
+++ Laboratory Activities/Laboratory Activity 1/alternatingChars.cs
@@ -0,0 +1,28 @@
+using System;
+
+class AlternatingChars
+{
+    static void Main()
+    {
+        Console.Write("First character: ");
+        char firstChar = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        
+        Console.Write("Second character: ");
+        char secondChar = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        
+        Console.Write("Enter size: ");
+        int size = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(new string('-', i)); 
+
+            if (i % 2 == 0)
+                Console.WriteLine(firstChar);
+            else
+                Console.WriteLine(secondChar);
+        }
+    }
+}
